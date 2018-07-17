91ec5933fc9165989c34d5cf3158a92f7d2b88e9
Zor90 <34856483+Zor90@users.noreply.github.com>
2018-07-17 19:34:35 +0200

    help

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,11 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+var prefix = ("*");
+
 client.on('ready', () => {
+	client.user.setGame("Command: *help");
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
+  if (msg.content === prefix + 'help') {
+    msg.channel.sendMessage('Liste des commandes: \n -*help');
+  }
   if (msg.content === 'ping' || msg.content === 'Ping' || msg.content === 'PING' ) {
     msg.reply('Pong!');
   }
