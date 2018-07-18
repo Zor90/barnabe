3e8e80309f8e6b67108214ab7f513e4fa7123f44
Zor90 <34856483+Zor90@users.noreply.github.com>
2018-07-18 15:20:32 +0200

    detect emoji

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-var prefix = (":");
+var prefix = ("*");
 
 client.on('ready', () => {
 	client.user.setGame("Qu'est ce que je vous sers?");
@@ -32,12 +32,12 @@ client.on('message', msg => {
   if (msg.content === 'Barnabe' || msg.content === 'barnabe') {
     msg.reply('Je suis là!');
   }
-  if (msg.content === prefix + 'joy' + prefix) {
-	msg.send ('test');}		
-  if (msg.content === prefix + 'sweat_smile' + prefix) {
-	msg.send ("Hoho, l'engoisse...");}	
-  if (msg.content === prefix + 'heart_eyes' + prefix) {
-	msg.send ("Berk c'est dégueu!");}
+  if (msg.content.startsWith (":joy:")) {
+	msg.channel.send ("HAHA, c'était marrant ça " + member.displayName + "!");}		
+  if (msg.content.startsWith (":sweat_smile:")) {
+	msg.channel.send ("Hoho, l'engoisse...");}	
+  if (msg.content.startsWith (":heart_eyes:")) {
+	msg.channel.send ("Berk c'est dégueu!");}
 });
 
 function random(min, max){
