8f83303f1d6f74fb5ec6e0fa1b37901804625f9c
TAMIGNIAUX Julien <Julien.TAMIGNIAUX@veibe.net>
2018-07-23 16:46:55 +0200

    corr start

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,10 +13,10 @@ client.on('message', msg => {
   if (msg.content === prefix + 'help') {
     msg.channel.sendMessage('Liste des commandes: \n -*help');
   }
-  if (msg.content.startsWith === 'ping' || msg.content.startsWith === 'Ping' || msg.content.startsWith === 'PING' ) {
+  if (msg.content.startsWith  ('ping') || msg.content.startsWith  ('Ping') || msg.content.startsWith  ('PING') ) {
     msg.reply('Pong!');
   }
-  if (msg.content.startsWith === 'bonjour' || msg.content.startsWith === 'Bonjour' || msg.content.startsWith === 'Pwet' || msg.content.startsWith === 'salut' || msg.content.startsWith === 'Salut' || msg.content.startsWith === 'hello' || msg.content.startsWith === 'Coucou' || msg.content.startsWith === 'coucou' || msg.content.startsWith === 'Nyu') {
+  if (msg.content.startsWith  ('bonjour') || msg.content.startsWith  ('Bonjour') || msg.content.startsWith  ('Pwet') || msg.content.startsWith  ('salut') || msg.content.startsWith  ('Salut') || msg.content.startsWith  ('hello') || msg.content.startsWith  ('Coucou') || msg.content.startsWith  ('coucou') || msg.content.startsWith  ('Nyu') {
     random();
 	if (randnum == 1) {
 		msg.reply('Je vous sers une chopine?');}
@@ -27,7 +27,7 @@ client.on('message', msg => {
 	if (randnum == 4) {
 		msg.reply('VIITE, un autre tonneau!');}
   }
-  if (msg.content.startsWith === "irashai") {
+  if (msg.content.With ("irashai") {
     msg.reply('Oula keskidit lui Oo');
   }
   if (msg.content.startsWith ('Barnabe') || msg.content.startsWith ('Barnabé') || msg.content.startsWith ('barnabe') || msg.content.startsWith ('barnabé')) {
