8e649590752d88c5e6a6b07593403dd02eb9c0cc
TAMIGNIAUX Julien <Julien.TAMIGNIAUX@veibe.net>
2018-07-23 16:51:57 +0200

    oups

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,7 +16,7 @@ client.on('message', msg => {
   if (msg.content.startsWith  ('ping') || msg.content.startsWith  ('Ping') || msg.content.startsWith  ('PING') ) {
     msg.reply('Pong!');
   }
-  if (msg.content.startsWith  ('bonjour') || msg.content.startsWith  ('Bonjour') || msg.content.startsWith  ('Pwet') || msg.content.startsWith  ('salut') || msg.content.startsWith  ('Salut') || msg.content.startsWith  ('hello') || msg.content.startsWith  ('Coucou') || msg.content.startsWith  ('coucou') || msg.content.startsWith  ('Nyu') {
+  if (msg.content.startsWith  ('bonjour') || msg.content.startsWith  ('Bonjour') || msg.content.startsWith  ('Pwet') || msg.content.startsWith  ('salut') || msg.content.startsWith  ('Salut') || msg.content.startsWith  ('hello') || msg.content.startsWith  ('Coucou') || msg.content.startsWith  ('coucou') || msg.content.startsWith  ('Nyu')) {
     random();
 	if (randnum == 1) {
 		msg.reply('Je vous sers une chopine?');}
